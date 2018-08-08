21cc08bb145555d0a31d3d2bcac9142eb4dd97e0
Sergey Lazarenkov <real3060@gmail.com>
2018-08-08 22:02:59 +0300

    выполнил сложное ДЗ по уроку №1

---
+++ lesson_1/hard/script.js
@@ -0,0 +1,9 @@
+var num1 = "3";
+var num2 = "3";
+var num3 = "7";
+var num4 = "2";
+var num5 = "1";
+
+console.log(+(num1 * num2 * num3 * num4 * num5));
+var summ = +(num1 * num2 * num3 * num4 * num5);
+alert(((Math.pow(summ, 3)).toString()).slice(0, 2));
