c5ff34c21ec5de89979dd19730ee116d04c37738
Sergey Lazarenkov <real3060@gmail.com>
2018-08-09 18:29:56 +0300

    сделал сложное задание с массива начинающихся на ""

--- lesson_2/дз/ДОПОЛНИТЕЛЬНОЕ (УСЛОЖНЕННОЕ) ЗАДАНИЕ/script3.js
+++ lesson_2/дз/ДОПОЛНИТЕЛЬНОЕ (УСЛОЖНЕННОЕ) ЗАДАНИЕ/script3.js
@@ -1,6 +1,21 @@
 let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
-for (let i = 0; i < 7; i++) {
-	// console.log(week[i]);
-	console.log('<b>' + (week[i]) + '</b>');
-	+ (week).slice(5)
+for (let i=0; i< week.length; i++) {
+    if(i == 5 || i == 6){
+        document.write("<b>" + week[i] +"</b><br>");
+
+    } else  if (i == 3) {
+        document.write('<i>' + week[i] +"</i><br>");
+    } else {
+        document.write(week[i] + "</br>")
+    }
 };
+
+
+var arr = ['14790873429579','875859803840587', '323471987491283740','4939273450927345','325723945872350','7987324590277690375','23412341234132413255'];
+
+for (var i=0; i<arr.length; i++){
+    if (arr[i][0] == "3" || arr[i][0] == "7") {
+        console.log(arr[i]);
+    }
+    
+}
