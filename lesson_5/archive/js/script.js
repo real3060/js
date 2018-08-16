ec57276ddc48425d7f17b07ae86a2100d7ea0904
Sergey Lazarenkov <real3060@gmail.com>
2018-08-16 17:40:51 +0300

    сделано 1 простое дз

--- lesson_5/archive/js/script.js
+++ lesson_5/archive/js/script.js
@@ -1,12 +1,35 @@
 let item = document.querySelectorAll('.menu-item'),
-	background = document.getElementsByTagName('body'),
+	background = document.getElementsByTagName('body'), /* чтобы обратиться используем  background[0]  */
 	menu = document.querySelector('.menu'),
+	// создание li
 	li = document.createElement('li'),
-	text = document.createTextNode('Пятый пункт');
+	// ищу элемент с тегом #title 
+	chH = document.getElementById('title'),
+	// создаем переменную для замены этим текстом, другой текст в chH
+	text = document.createTextNode('Пятый пункт'),
+	// ищем элемент для того чтобы удалить дочерний элемент рекламы
+	deleted = document.querySelector(".adv"),
+	// поиск элемента на страницке с класом prompt
+	questionSearch = document.querySelector(".prompt"),
+	question = prompt(" Какое у Вас отношение к технике apple? ");
+	// замена элементов li местами
 menu.insertBefore(item[2], item[1]);
+// добавил класс для li class="menu-item"
 li.classList.add('menu-item');
+// добавление в меню нашего li 
 menu.appendChild(li);
+// добавление текста в li 
 li.appendChild(text);
-// bg.style.backgroundColor = "black";
-document.body.style.backgroundImage = "url(/img/apple_true.jpg)";
-	// document.body.style.backgroundColor = "black";
+// добавление картинки
+document.body.style.backgroundImage = "url(img/apple_true.jpg)";
+console.log('title');
+// меняем текст в нужном нам месте при помощи метода .textContent
+chH.textContent = "Мы продаем только подлинную технику Apple"
+
+// проверяем что выводит deleted
+console.log(deleted);
+// удаляем элемент рекламы
+deleted.remove();
+
+questionSearch.textContent = (question);
+
