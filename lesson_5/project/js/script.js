36b3e9ece6916a600935bf34615a4a6f2fe41697
Sergey Lazarenkov <real3060@gmail.com>
2018-08-16 19:55:03 +0300

    правка 5го

--- lesson_5/project/js/script.js
+++ lesson_5/project/js/script.js
@@ -7,18 +7,15 @@ let btn = document.getElementById('open-btn'),
 	leftDiscount = document.querySelector(".discount-value"),
 	leftIsOpen = document.querySelector(".isopen-value");
 
-	// все поля категории
+// все поля категории
 let getCategory = document.querySelectorAll(".goods-item");
 
 
-	// все 3 кнопки через tag
+// все 3 кнопки через tag
 let btn1 = document.getElementsByTagName("button");
 
 // Получить поля ввода товаров  через querySelector
-let inputType1 = document.querySelector('.goods-item');
-let inputType2 = document.querySelector('.goods-item');
-let inputType3 = document.querySelector('.goods-item');
-let inputType4 = document.querySelector('.goods-item');
+let inputType = document.querySelectorAll('.goods-item');
 // времени через querySelector
 let whatsTime = document.querySelector(".time-value");
 // бюджета через querySelector
@@ -33,10 +30,10 @@ console.log(btn1[2]);
 console.log(btn1[3]);
 console.log(leftIsOpen);
 console.log(getCategory);
-console.log(inputType1);
-console.log(inputType2);
-console.log(inputType3);
-console.log(inputType4);
+console.log(inputType[1]);
+console.log(inputType[2]);
+console.log(inputType[3]);
+console.log(inputType[4]);
 console.log(whatsTime);
 console.log(employersValue[0]);
 console.log(employersValue[1]);
