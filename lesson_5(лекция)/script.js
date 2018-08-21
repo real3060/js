7711081258d7c326a5760519269b3dc063abec85
Sergey Lazarenkov <real3060@gmail.com>
2018-08-21 08:50:03 +0300

    правки переменной

--- lesson_5(лекция)/script.js
+++ lesson_5(лекция)/script.js
@@ -12,11 +12,11 @@
 
 
 
-nextPrime:
+fori:
 for (var i = 2; i <= 100; i++) {
 
 for (var j = 2; j < i; j++) {
-	if (i % j == 0) continue nextPrime;
+	if (i % j == 0) continue fori;
 }
 document.write("<p>"+ i +" - делители этого числа: " + "1" + " и "+ j +"</p>") 
 }
