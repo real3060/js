32e5be8b87c9a2788157f11a0938c880a378be3a
Sergey Lazarenkov <real3060@gmail.com>
2018-08-24 14:07:09 +0300

    сделал простое 9е

---
+++ lesson_9/АРХИВ ДЛЯ ДЗ/js/script.js
@@ -0,0 +1,114 @@
+window.addEventListener('DOMContentLoaded', function() {
+
+	let tab = document.getElementsByClassName('info-header-tab'),
+		tabContent = document.getElementsByClassName('info-tabcontent'),
+		info = document.getElementsByClassName('info-header')[0];
+
+		function hideTabContent(a) {
+			for (let i =a; i<tabContent.length; i++){
+				tabContent[i].classList.remove('show');
+				tabContent[i].classList.add('hide');
+			}
+		}
+
+		hideTabContent(1);
+function showTabContent (b) {
+	if (tabContent[b].classList.contains('hide')){
+		hideTabContent(0);
+		tabContent[b].classList.remove('hide');
+		tabContent[b].classList.add('show');
+
+	}
+}
+	info.addEventListener('click', function(event) {
+		let target = event.target;
+		if (target.className == 'info-header-tab'){
+			for(let i = 0; i<tab.length; i++){
+				if(target == tab[i]){
+					showTabContent(i);
+					break;
+				}
+			}
+		}
+	});
+	//  timer
+	let deadLine = '2018-08-23';
+	// расчет времени в милисекундах от того времени которое задано
+
+	
+	// расчет времени в милисекундах от того времени которое задано
+	function getTimeRemaining (endtime) {
+		let t = Date.parse(endtime) - Date.parse(new Date()),
+		seconds = Math.floor((t/1000) % 60),
+		minutes = Math.floor( (t/1000/60) %60),
+		hours = Math.floor( (t/(1000*60*60)) );
+	
+		return{
+			'total': t,
+			'hours': hours,
+			'minutes': minutes,
+			'seconds': seconds,
+		};
+	};
+function setClock(id, endtime) {
+	let timer =document.getElementById(id),
+		hours = timer.querySelector('.hours'),
+		minutes = timer.querySelector('.minutes'),
+		seconds = timer.querySelector('.seconds');
+		
+		function updateClock() {
+			let t = getTimeRemaining(endtime);
+
+			hours.innerHTML = t.hours;
+			minutes.innerHTML = t.minutes;
+			seconds.innerHTML = t.seconds;
+
+			if(t.total == 0) {
+				clearInterval(timeInterval);
+			} 
+			if (t.total< 0) {
+				hours.innerHTML = "00";
+				minutes.innerHTML = "00";
+				seconds.innerHTML = "00";
+			}
+		};
+
+		updateClock();
+		let timeInterval = setInterval(updateClock, 1000);
+		
+}
+
+setClock('timer', deadLine);
+// модалка 
+	let more = document.querySelector('.more');
+	let overlay = document.querySelector('.overlay');
+	let close = document.querySelector('.popup-close');
+
+	more.addEventListener('click', function(){
+		this.classList.add('more-splash');
+		overlay.style.display = "block";
+		document.body.style.overflow = "hidden";
+		
+	});
+	close.addEventListener('click', function() {
+		overlay.style.display = 'none';
+		more.classList.remove('more-splash');
+		document.body.style.overflow = "";
+	});
+
+	// модалка на кнопку узнать подробнее
+	let knowMore = document.querySelectorAll('.description-btn');
+	for ( let i =0; i<4; i++)
+	 knowMore[i].addEventListener('click', function() {
+		this.classList.add('more-splash');
+		overlay.style.display = 'block';
+		document.body.style.overflow = 'hidden';
+	});
+	
+
+
+});
+
+
+
+
---
+++ lesson_9/дз/js.js
@@ -0,0 +1,7 @@
+let age = document.getElementById('age').value;
+
+function showUser(surname, name) {
+		this.value = age;
+        console.log("Пользователь " + surname + " " + name + ", еговозраст " + this.value);
+}
+showUser("Mr", 'John');
