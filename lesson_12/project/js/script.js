45d7158c030913a9f7ff87b4e01f27ddd4e1590a
Sergey Lazarenkov <real3060@gmail.com>
2018-08-28 19:54:43 +0300

    пробная версия 12 простого задания

--- lesson_12/project/js/script.js
+++ lesson_12/project/js/script.js
@@ -254,7 +254,6 @@ window.addEventListener('DOMContentLoaded', () => {
 
 
 	//slider
-
 	let slideIndex = 1,
 		slides = document.getElementsByClassName('slider-item'),
 		prev = document.querySelector(".prev"),
@@ -262,22 +261,96 @@ window.addEventListener('DOMContentLoaded', () => {
 		dotsWrap = document.querySelector('.slider-dots'),
 		dots = document.getElementsByClassName('dot');
 
-		showSlides(slideIndex);
-		function showSlides(n) {
-			if(n>slides.length){
-				slideIndex = 1;
-			};
-			if(n<1) {
-				slideIndex = slides.length;
-			};
-			for(let i = 0; i< slides.length; i++) {
-				slides[i].style.display = 'none';
-			};
-
-			for( let i =0; i <dot.length; i++){
-				dots[i].classList.remove("dot-active");
-			};
-			slides[slideIndex - 1].style.display = "block";
-			dots[slideIndex - 1].style.classList.add('dot-active')
+	showSlides(slideIndex);
+	function showSlides(n) {
+		if (n > slides.length) {
+			slideIndex = 1;
 		};
+		if (n < 1) {
+			slideIndex = slides.length;
+		};
+		for (let i = 0; i < slides.length; i++) {
+			slides[i].style.display = 'none';
+		};
+
+		for (let i = 0; i < dots.length; i++) {
+			dots[i].classList.remove("dot-active");
+		};
+		slides[slideIndex - 1].style.display = "block";
+		dots[slideIndex - 1].classList.add('dot-active');
+	};
+
+	function plusSlides(n) {
+		showSlides(slideIndex += n)
+	};
+
+	function currentSlide(n) {
+		showSlides(slideIndex = n);
+	};
+
+	prev.addEventListener('click', function () {
+		plusSlides(-1);
+	});
+	next.addEventListener('click', function () {
+		plusSlides(1);
+	});
+
+	dotsWrap.addEventListener('click', function (event) {
+		for (let i = 0; i < dots.length + 1; i++) {
+			if (event.target.classList.contains('dot') && event.target == dots[i - 1]) {
+				currentSlide(i);
+			}
+		}
+	});
+
+	//calc
+
+	let persons = document.getElementsByClassName('counter-block-input')[0],
+		restDays = document.getElementsByClassName('counter-block-input')[1],
+		place = document.getElementById('select'),
+		totalValue = document.getElementById('total'),
+		personsSum = 0,
+		daysSum = 0,
+		total = 0;
+	totalValue.innerHTML = 0;
+
+	persons.addEventListener('change', function () {
+		personsSum = +this.value
+		total = (daysSum + personsSum) * 4000;
+		if (restDays.value == "") {
+			totalValue.innerHTML = 0;
+		} else {
+			totalValue.innerHTML = total;
+		};
+		//символы
+		persons.onkeyup = function(e) {
+			let errS = "+e,.E";
+			let key = String.fromCharCode(e.which);
+			if(errS.indexOf(key) >= 0){
+				console.log('invalid key pressed');    
+				return false;
+			}
+			return true;    
+		};
+	});
+	restDays.addEventListener('change', function () {
+		daysSum = +this.value
+		total = (daysSum + personsSum) * 4000;
+		if (persons.value == "" ) {
+			totalValue.innerHTML = 0;
+		} else {
+			totalValue.innerHTML = parseInt(total);
+		}
+	});
+
+	place.addEventListener('change', function () {
+		if (persons.value == "" || restDays.value == "") {
+			totalValue.innerHTML = 0
+		} else {
+			let a = total;
+			totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+		}
+	})
+
 });
+
