0462224acbf9912a727c5c854150ee05c9abe2c7
Sergey Lazarenkov <real3060@gmail.com>
2018-09-03 19:35:55 +0300

    правки 14 и правка калькулятора

--- lesson_14/project/js/bundle/bundle.js
+++ lesson_14/project/js/bundle/bundle.js
@@ -97,58 +97,56 @@
 				place = document.getElementById('select'),
 				totalValue = document.getElementById('total'),
 				personsSum = 0,
-				daysSum = 0,
+				daysSum = 1,
 				total = 0;
 			totalValue.innerHTML = 0;
+
+
 			persons.addEventListener('change', function () {
 				personsSum = +this.value;
 				total = (daysSum + personsSum) * 4000;
 
-				if (restDays.value == "") {
+				if (persons.value == "" || persons.value == "0" || restDays.value == "" || restDays.value == "0") {
 					totalValue.innerHTML = 0;
+				
 				} else {
-					totalValue.innerHTML = total;
+					totalValue.innerHTML = parseInt(total);
 				}
 
-				; //символы
-
-				persons.onkeyup = function (e) {
-					var key = String.fromCharCode(e.which);
-
-					if (errS.indexOf(key) >= 0) {
-						console.log('invalid key pressed');
-						return false;
-					}
-
-					return true;
-				};
 			});
+
+
 			restDays.addEventListener('change', function () {
 				daysSum = +this.value;
 				total = (daysSum + personsSum) * 4000;
 
-				if (persons.value == "") {
+				if (persons.value == "" || persons.value == "0" || restDays.value == "" || restDays.value == "0") {
 					totalValue.innerHTML = 0;
 				} else {
 					totalValue.innerHTML = parseInt(total);
 				}
 			});
+
+
 			place.addEventListener('change', function () {
 				if (persons.value == "" || restDays.value == "") {
-					totalValue.innerHTML = 0;
+					if (persons.value == "0" || restDays == "0") {
+						totalValue.innerHTML = 0;
+					}
 				} else {
 					var a = total;
 					totalValue.innerHTML = a * this.options[this.selectedIndex].value;
 				}
-			}); // замена символов restDays
+			});
+			// замена символов restDays
 
 			restDays.onkeyup = function () {
-				return this.value = this.value.replace(/[\D{3}]/g, '');
+				return this.value = this.value.replace(/[\D]/g, '');
 			}; // замена символов  persons
 
 
 			persons.onkeyup = function () {
-				return this.value = this.value.replace(/[\D{3}]/g, '');
+				return this.value = this.value.replace(/[\D]/g, '');
 			};
 		}
 
--- lesson_14/project/js/parts/calc.js
+++ lesson_14/project/js/parts/calc.js
@@ -1,63 +1,51 @@
 function calc() {
-  //calc
-  var persons = document.getElementsByClassName('counter-block-input')[0],
-      restDays = document.getElementsByClassName('counter-block-input')[1],
-      place = document.getElementById('select'),
-      totalValue = document.getElementById('total'),
-      personsSum = 0,
-      daysSum = 0,
-      total = 0;
-  totalValue.innerHTML = 0;
-  persons.addEventListener('change', function () {
-    personsSum = +this.value;
-    total = (daysSum + personsSum) * 4000;
+	//calc
+	var persons = document.getElementsByClassName('counter-block-input')[0],
+		restDays = document.getElementsByClassName('counter-block-input')[1],
+		place = document.getElementById('select'),
+		totalValue = document.getElementById('total'),
+		personsSum = 0,
+		daysSum = 0,
+		total = 0;
+	totalValue.innerHTML = 0;
+	persons.addEventListener('change', function () {
+		personsSum = +this.value;
+		total = (daysSum + personsSum) * 4000;
 
-    if (restDays.value == "") {
-      totalValue.innerHTML = 0;
-    } else {
-      totalValue.innerHTML = total;
-    }
+		if (restDays.value == "") {
+			totalValue.innerHTML = 0;
+		} else {
+			totalValue.innerHTML = total;
+		}
 
-    ; //символы
+	});
+	restDays.addEventListener('change', function () {
+		daysSum = +this.value;
+		total = (daysSum + personsSum) * 4000;
 
-    persons.onkeyup = function (e) {
-      var key = String.fromCharCode(e.which);
+		if (persons.value == "" || persons.value === "0") {
+			totalValue.innerHTML = 0;
+		} else {
+			totalValue.innerHTML = parseInt(total);
+		}
+	});
+	place.addEventListener('change', function () {
+		if (persons.value == "" || restDays.value == "" || restDays.value === "0") {
+			totalValue.innerHTML = 0;
+		} else {
+			var a = total;
+			totalValue.innerHTML = a * this.options[this.selectedIndex].value;
+		}
+	}); // замена символов restDays
 
-      if (errS.indexOf(key) >= 0) {
-        console.log('invalid key pressed');
-        return false;
-      }
-
-      return true;
-    };
-  });
-  restDays.addEventListener('change', function () {
-    daysSum = +this.value;
-    total = (daysSum + personsSum) * 4000;
-
-    if (persons.value == "") {
-      totalValue.innerHTML = 0;
-    } else {
-      totalValue.innerHTML = parseInt(total);
-    }
-  });
-  place.addEventListener('change', function () {
-    if (persons.value == "" || restDays.value == "") {
-      totalValue.innerHTML = 0;
-    } else {
-      var a = total;
-      totalValue.innerHTML = a * this.options[this.selectedIndex].value;
-    }
-  }); // замена символов restDays
-
-  restDays.onkeyup = function () {
-    return this.value = this.value.replace(/[\D{3}]/g, '');
-  }; // замена символов  persons
+	restDays.onkeyup = function () {
+		return this.value = this.value.replace(/[\D]/g, '');
+	}; // замена символов  persons
 
 
-  persons.onkeyup = function () {
-    return this.value = this.value.replace(/[\D{3}]/g, '');
-  };
+	persons.onkeyup = function () {
+		return this.value = this.value.replace(/[\D]/g, '');
+	};
 }
 
 module.exports = calc;
